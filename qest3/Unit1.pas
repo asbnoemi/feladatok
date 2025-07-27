24f2037e580de18c343763ff5f0c2e613f5d6b15
info projekt 1 <asboth.nono@gmail.com>
2025-07-27 20:24:25 +0200

    Add files via upload

---
+++ qest3/Unit1.pas
@@ -0,0 +1,60 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, IBX.IBCustomDataSet,
+  IBX.IBTable, IBX.IBDatabase, Vcl.Grids, Vcl.DBGrids, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    IBDatabase1: TIBDatabase;
+    IBTable1: TIBTable;
+    DataSource1: TDataSource;
+    DBGrid1: TDBGrid;
+    IBTransaction1: TIBTransaction;
+    DeletBT: TButton;
+    Button2: TButton;
+
+    procedure IBTable1BeforePost(DataSet: TDataSet);
+    procedure DeletBTClick(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+IBTable1.Insert();
+end;
+
+procedure TForm1.DeletBTClick(Sender: TObject);
+begin
+IBTable1.Delete();
+end;
+
+procedure TForm1.IBTable1BeforePost(DataSet: TDataSet);
+begin
+
+  if(mrNo= MessageDlg('biztos',TMsgDlgType.mtConfirmation,[TMsgDlgBtn.mbYes, TMsgDlgBtn.mbNo],0))
+then
+begin
+if DataSet.State in [dsEdit, dsInsert] then
+DataSet.Cancel();
+Abort;
+end ;
+end;
+
+end.
